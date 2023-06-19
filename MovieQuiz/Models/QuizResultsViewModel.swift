74bfb886586f9cd7322588682f79de4dcb6909f6
Dark2801 <ripper2801@mail.ru>
2023-06-19 11:10:05 +0300

    Создал папку Models с файлами: QuizeStepViewModel, QuizeResultsViewModel, QuizeQuestion.

---
+++ MovieQuiz/Models/QuizResultsViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  QuizResultsViewModel.swift
+//  MovieQuiz
+//
+//  Created by Андрей Мерзликин on 19.06.2023.
+//
+
+import Foundation
+import UIKit
+
+struct QuizResultsViewModel {
+  let title: String
+  let text: String
+  let buttonText: String
+}
